188bc67413e77b3800b1937926d7cd8ae9292160
tanmaisai1208 <tanmaisaich@gmail.com>
2026-09-09 23:13:15 +0530

    Refactor snake-and-ladder game logic and cleanup
    Removed unnecessary output statements and cleaned up the Player class by removing displayInfo method. Simplified the Game class by removing unused methods and adjusted the Dice constructor.

--- snake-and-ladder.cpp
+++ snake-and-ladder.cpp
@@ -20,12 +20,10 @@ public:
     int getNextPosition(int currentPos) const {
         auto snakeIt = snakes.find(currentPos);
         if (snakeIt != snakes.end()) {
-            cout << "Oops! Snake at position " << currentPos << ". Moving down to " << snakeIt->second << endl;
             return snakeIt->second;
         }
         auto ladderIt = ladders.find(currentPos);
         if (ladderIt != ladders.end()) {
-            cout << "Yay! Ladder at position " << currentPos << ". Moving up to " << ladderIt->second << endl;
             return ladderIt->second;
         }
         return currentPos;
@@ -40,9 +38,10 @@ class Dice {
 private:
     int sides;
 public:
-    Dice(int sides = 6) : sides(sides) {
-        srand(static_cast<unsigned int>(time(nullptr)));
+    Dice(int sides = 6) {
+        
     }
+
     int roll() const {
         return (rand() % sides) + 1;
     }
@@ -58,14 +57,8 @@ public:
     Player(string name) : name(move(name)), position(0), winner(false) {}
     string getName() const { return name; }
     int getPosition() const { return position; }
-    bool isWinner() const { return winner; }
     void setPosition(int pos) { position = pos; }
     void setWinner(bool w) { winner = w; }
-    void displayInfo() const {
-        cout << "Player: " << name << endl;
-        cout << "Position: " << position << endl;
-        cout << "Status: " << (winner ? "Winner!" : "Playing") << endl;
-    }
 };
 
 // ----- Game class -----
@@ -85,49 +78,25 @@ public:
     void setupBoard() {
         // input about snakes and ladder positions would be given and we store them
     }
-    Player* getCurrentPlayer() const { return players[currentPlayerIndex]; }
     void movePlayer(Player *player, int steps) {
         int newPos = player->getPosition() + steps;
         if (newPos > board.getSize()) {
-            cout << "Cannot move, need exact number to finish" << endl;
             return;
         }
         newPos = board.getNextPosition(newPos);
         player->setPosition(newPos);
-        cout << player->getName() << " moved to position " << newPos << endl;
     }
     bool checkWin(Player *player) const { return player->getPosition() == board.getSize(); }
     bool makeMove() {
-        if (gameOver || players.empty()) return false;
+        if (gameOver) return false;
         Player *cur = getCurrentPlayer();
-        cout << "\n" << cur->getName() << "'s turn" << endl;
         int roll = dice.roll();
-        cout << "Rolled: " << roll << endl;
         movePlayer(cur, roll);
         if (checkWin(cur)) {
             cur->setWinner(true);
             gameOver = true;
-            cout << "\nCongratulations! " << cur->getName() << " wins!" << endl;
-            return false;
         }
         currentPlayerIndex = (currentPlayerIndex + 1) % players.size();
         return true;
     }
-    void displayStatus() const {
-        
-    }
-    void play() {
-        if (players.empty()) {
-            cout << "No players in the game!" << endl;
-            return;
-        }
-        cout << "\nStarting the game..." << endl;
-        while (makeMove()) {
-            displayStatus();
-            this_thread::sleep_for(chrono::seconds(1));
-        }
-        displayStatus();
-    }
 };
-
-
